b2c180d6f453a1c0548112398e1c4aba921438f1
Jeev Pangli <jspanglez@gmail.com>
2022-12-06 19:25:27 +0000

    Added DataBaseHelper, not finished

---
+++ app/src/main/java/com/example/surveyapp/Model/DataBaseHelper.kt
@@ -0,0 +1,91 @@
+package com.example.surveyapp.Model
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteOpenHelper
+
+/* Database Config*/
+private val DataBaseName = "surveyDatabase.db"
+private val ver : Int = 1
+
+class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DataBaseName, null, ver) {
+
+    // User Table
+    private val TableName = "Users"
+
+    private val Column_UserID = "UserID"
+    private val Column_Username = "username"
+    private val Column_Password = "password"
+    private val Column_isAdmin = "isAdmin"
+
+    // Create the database
+    override fun onCreate(db: SQLiteDatabase?) {
+        try {
+            val sqlCreateStatement: String = "CREATE TABLE " + TableName + " ( " + Column_UserID +
+                    " INTEGER PRIMARY KEY AUTOINCREMENT, " + Column_Username + " TEXT NOT NULL UNIQUE, " +
+                    Column_Password + " TEXT NOT NULL, " + Column_isAdmin + " INTEGER NOT NULL DEFAULT 0 )"
+
+            db?.execSQL(sqlCreateStatement)
+        }
+        catch (e: SQLiteException) {}
+    }
+
+    private fun checkUsername(user: User): Int {
+
+        val db: SQLiteDatabase
+        try {
+            db = this.readableDatabase
+        }
+        catch(e: SQLiteException) {
+            return -2
+        }
+
+        val username = user.username.lowercase()
+
+        val sqlStatement = "SELECT * FROM $TableName WHERE $Column_Username = ?"
+        val param = arrayOf(username)
+        val cursor: Cursor =  db.rawQuery(sqlStatement,param)
+
+        if(cursor.moveToFirst()){
+            // The user is found
+            val n = cursor.getInt(0)
+            cursor.close()
+            db.close()
+            return -3 // error the user name already exists
+        }
+
+        cursor.close()
+        db.close()
+        return 0 //User not found
+
+    }
+
+    fun addUser (user: User) : Int {
+
+        val doesUsernameExist = checkUsername(user)
+        if(doesUsernameExist < 0)
+            return doesUsernameExist
+
+        val db: SQLiteDatabase = this.writableDatabase
+        val cv: ContentValues = ContentValues()
+
+        cv.put(Column_Username, user.username.lowercase())
+        cv.put(Column_Password, user.password)
+        cv.put(Column_isAdmin, user.isAdmin)
+
+        val success = db.insert(TableName, null, cv)
+
+        db.close()
+
+        if (success.toInt() == -1)
+            return success.toInt() //Error, adding new user
+        else
+            return 1
+    }
+
+
+}
+
