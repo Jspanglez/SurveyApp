714354a3fbe62a496bb3502aaea160e7f974a477
Jeev Pangli <jspanglez@gmail.com>
2022-12-14 21:23:56 +0000

    Made an activity for editing surveys (no functionality)

---
+++ app/src/main/java/com/example/surveyapp/EditSurveyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.surveyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditSurveyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_survey)
+    }
+}
