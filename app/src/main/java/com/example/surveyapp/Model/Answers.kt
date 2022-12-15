7cc564011583ae9d196b581a2004e6a3c592dd14
Jeev Pangli <jspanglez@gmail.com>
2022-12-15 19:02:56 +0000

    Changed "text" to "answerText"

--- app/src/main/java/com/example/surveyapp/Model/Answers.kt
+++ app/src/main/java/com/example/surveyapp/Model/Answers.kt
@@ -1,4 +1,4 @@
 package com.example.surveyapp.Model
 
-data class Answers(val AnswerID: Int, val text: String, val QuestionID: Int, val UserID: Int) {
+data class Answers(val AnswerID: Int, val answerText: String, val QuestionID: Int, val UserID: Int) {
 }
