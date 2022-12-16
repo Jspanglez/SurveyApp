6242dd8ab16b1713fbc10f9e2afc399d84bfc9f2
Jeev Pangli <jspanglez@gmail.com>
2022-12-16 19:52:15 +0000

    Added functions for buttons (no functionality)

--- app/src/main/java/com/example/surveyapp/CreateSurveyActivity.kt
+++ app/src/main/java/com/example/surveyapp/CreateSurveyActivity.kt
@@ -1,11 +1,28 @@
 package com.example.surveyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CreateSurveyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_survey)
+    }
+
+    fun nextBtn(view: View) {
+        //Make validation here
+        //if something is not selected: make a toast saying "Please complete all fields."
+
+
+        //Push Survey Title, Start Date and End date to the database here
+        /*val intentNext = Intent(this, CreateQuestionsActivity::class.java)
+        startActivity(intentNext)*/
+    }
+
+    fun backBtn(view: View) {
+        val intentBack = Intent(this, AdminHomeActivity::class.java)
+        startActivity(intentBack)
     }
 }
