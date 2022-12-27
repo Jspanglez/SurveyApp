29f2fcf28e8be5c8dc2e4d5a27eb34bb3b1932a0
Jeev Pangli <jspanglez@gmail.com>
2022-12-27 13:45:53 +0000

    Called database helper functions

--- app/src/main/java/com/example/surveyapp/AdminHomeActivity.kt
+++ app/src/main/java/com/example/surveyapp/AdminHomeActivity.kt
@@ -4,11 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ListView
+import com.example.surveyapp.Model.DataBaseHelper
 
 class AdminHomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_home)
+
+        var surveyList: ListView
+        val myDatabase = DataBaseHelper(this)
+
+        var surveyTitles = myDatabase.getSurveyTitle()
+        var startDateList = myDatabase.getStartDate()
+        var endDateList = myDatabase.getEndDate()
+
+        val edit = findViewById<Button>(R.id.buttonEdit)
+        edit.isEnabled = false
+
+        surveyList = findViewById<ListView>(R.id.myListView)
+
+        surveyList.setOnItemClickListener { parent, view, position, id ->
+            edit.isEnabled = true
+        }
+
+        //Create the adapter
+        val customAdapter = CustomAdapter(applicationContext, surveyTitles, startDateList, endDateList)
+        surveyList!!.adapter = customAdapter
     }
 
     fun createBtn(view: View) {
