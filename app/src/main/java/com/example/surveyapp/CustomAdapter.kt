cd508cf477c52efb349bef33c22d963f00a3c505
Jeev Pangli <jspanglez@gmail.com>
2022-12-27 13:43:05 +0000

    Created a custom adapter for the listview

---
+++ app/src/main/java/com/example/surveyapp/CustomAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.surveyapp
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+
+class CustomAdapter(private val appContext: Context, private val surveyTitles: ArrayList<String>,
+                    private val startDateList: ArrayList<String>, private val endDateList: ArrayList<String>
+) : BaseAdapter() {
+
+    private val inflater: LayoutInflater
+            = appContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount(): Int {
+        return surveyTitles.size
+    }
+
+    override fun getItem(i: Int): Any {
+        return i
+    }
+
+    override fun getItemId(i: Int): Long {
+        return i.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view: View?
+        view = inflater.inflate(R.layout.activity_listview, parent, false)
+
+        val titles = view.findViewById<TextView>(R.id.textViewTitle)
+        val sDate = view.findViewById<TextView>(R.id.textViewStartDate)
+        val eDate = view.findViewById<TextView>(R.id.textViewEndDate)
+
+        titles.text = surveyTitles[position]
+        sDate.text = startDateList[position]
+        eDate.text = endDateList[position]
+
+        return view
+    }
+
+    //Create function for the intent
+    //val intent = Intent(this, EditSurveyActivity::class.java)
+
+}
